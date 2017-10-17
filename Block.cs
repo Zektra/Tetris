7367d49ae7cdada35384d601832fefc1b3e812f8
Kevin Willemse <mickeyspiggy@gmail.com>
2017-10-17 16:51:03 +0200

    Mirco

---
+++ Block.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Block
+{
+    int WhichOne;
+
+    public Block(int WhichOne)
+    {
+        this.WhichOne = WhichOne;
+        CreateBlock();
+    }
+
+    public void CreateBlock()
+    {
+        bool[,] block = new bool[4, 4];
+        switch (WhichOne)
+        {
+            //I-Shape
+            case 0:
+                block[1, 2] = true;
+                block[2, 2] = true;
+                block[3, 2] = true;
+                block[4, 2] = true;
+                break;
+
+            /*case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;*/
+        }
+        
+    }
+}
---
+++ TetrisGrid.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+/*
+ * a class for representing the Tetris playing grid
+ */
+class TetrisGrid
+{
+    Block block = new Block(0);
+
+    public TetrisGrid(Texture2D b)
+    {
+        gridblock = b;
+        position = Vector2.Zero;
+        this.Clear();
+        CreateGridArray();
+        
+
+
+    }
+
+    
+    int x, y;
+    Color Kleur = new Color(255, 255, 255, 200);
+    TetrisGrid grid;
+
+    public void CreateGridArray()
+    {
+        bool[,] GridCheck = new bool[Height, Width];
+        GridCheck[1, 5] = IETS;
+    }
+
+
+    /*
+     * sprite for representing a single grid block
+     */
+    Texture2D gridblock;
+
+    /*
+     * the position of the tetris grid
+     */
+    Vector2 position;
+
+    /*
+     * width in terms of grid elements
+     */
+    public int Width
+    {
+        get { return 12; }
+    }
+
+    /*
+     * height in terms of grid elements
+     */
+    public int Height
+    {
+        get { return 20; }
+    }
+
+ 
+
+
+    /*
+     * clears the grid
+     */
+    public void Clear()
+    {
+    }
+
+    /*
+     * draws the grid on the screen
+     */
+    public void Draw(GameTime gameTime, SpriteBatch s)
+    {
+ 
+        for (x = 0; x < Width; x++)
+        {
+            for (y = 0; y < Height; y++)
+            {
+                s.Draw(gridblock, (position + new Vector2(x * gridblock.Width, y * gridblock.Height)), Kleur);
+                
+            }
+        }
+    }
+
+
+}
+
