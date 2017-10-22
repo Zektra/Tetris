cb8c9c343ee16838d6e7366179f4da60e6d2f250
Kevin Willemse <mickeyspiggy@gmail.com>
2017-10-22 18:27:10 +0200

    Childs added

--- TetrisBlock.cs
+++ TetrisBlock.cs
@@ -5,63 +5,114 @@ using Microsoft.Xna.Framework.Graphics;
 /*
  * a class for representing the Tetris playing grid
  */
+class I : TetrisBlock
+{
+	public I(Texture2D b)
+		: base(b)
+	{
+	}
+}
+class S : TetrisBlock
+{
+	public S(Texture2D b)
+		: base(b)
+	{
+	}
+}
+class Z : TetrisBlock
+{
+	public Z(Texture2D b)
+		: base(b)
+	{
+	}
+}
+class J : TetrisBlock
+{
+	public J(Texture2D b)
+		: base(b)
+	{
+	}
+}
+class O : TetrisBlock
+{
+	public O(Texture2D b)
+		: base(b)
+	{
+	}
+}
+class L : TetrisBlock
+{
+	public L(Texture2D b)
+		: base(b)
+	{
+	}
+}
+class T : TetrisBlock
+{
+	public T(Texture2D b)
+		: base(b)
+	{
+	}
+}
+
+
 class TetrisBlock
 {
-    public TetrisBlock(Texture2D b)
-    {
-        gridblock = b;
-        position = Vector2.Zero;
-        this.Clear();
-    }
+	public TetrisBlock(Texture2D b)
+	{
+		gridblock = b;
+		position = Vector2.Zero;
+		this.Clear();
+	}
 
 	bool[,] TGrid;
 
-    /*
+	/*
      * sprite for representing a single grid block
      */
-    Texture2D gridblock;
+	Texture2D gridblock;
 
-    /*
+	/*
      * the position of the tetris grid
      */
-    Vector2 position;
+	Vector2 position;
 
-    /*
+	/*
      * Position of block
      */
-    public Vector2 Position
-    {
-        get { return position; }
-    }
+	public Vector2 Position
+	{
+		get { return position; }
+	}
 
-    /*
+	/*
      * width in terms of grid elements
      */
-    public int Width
-    {
-        get { return 4; }
-    }
+	public int Width
+	{
+		get { return 4; }
+	}
 
-    /*
+	/*
      * height in terms of grid elements
      */
-    public int Height
-    {
-        get { return 4; }
-    }
+	public int Height
+	{
+		get { return 4; }
+	}
 
-    /*
+	/*
      * clears the grid
      */
-    public void Clear()
-    {
-        TGrid = new bool[Width, Height];
-        Console.WriteLine("Reset");
+	public void Clear()
+	{
+		TGrid = new bool[Width, Height];
+		Console.WriteLine("Reset");
 		TGrid[1, 2] = true;
 		TGrid[0, 3] = true;
 		TGrid[1, 3] = true;
 		TGrid[2, 3] = true;
-    }
+	}
 
 	/*
 	 * Returns boolean of position in TGrid
@@ -69,39 +120,39 @@ class TetrisBlock
 	public bool Check(int x, int y)
 	{
 		return TGrid[y, x];
-    }
+	}
 
-    public void Down()
-    {
-        position += new Vector2(0, 1 * gridblock.Width);
-    }
-    public void Up()
-    {
-        position -= new Vector2(0, 1 * gridblock.Width);
-    }
-    public void Left()
-    {
-        if (position.X > 0)
-            position = position - new Vector2(1 * gridblock.Width, 0);
-    }
-    public void Right()
-    {
-        if (position.X + gridblock.Width < 9 * gridblock.Width)
-            position = position + new Vector2(1 * gridblock.Width, 0);
-    }
+	public void Down()
+	{
+		position += new Vector2(0, 1 * gridblock.Width);
+	}
+	public void Up()
+	{
+		position -= new Vector2(0, 1 * gridblock.Width);
+	}
+	public void Left()
+	{
+		if (position.X > 0)
+			position = position - new Vector2(1 * gridblock.Width, 0);
+	}
+	public void Right()
+	{
+		if (position.X + gridblock.Width < 9 * gridblock.Width)
+			position = position + new Vector2(1 * gridblock.Width, 0);
+	}
 
-    /*
+	/*
      * draws the grid on the screen
      */
-    public void Draw(GameTime gameTime, SpriteBatch s)
-    {
-        for (int x = 0; x < Width; x++)
-        {
-            for (int y = 0; y < Height; y++)
-            {
+	public void Draw(GameTime gameTime, SpriteBatch s)
+	{
+		for (int x = 0; x < Width; x++)
+		{
+			for (int y = 0; y < Height; y++)
+			{
 				if (TGrid[x, y])
 					s.Draw(gridblock, (position + new Vector2(x * gridblock.Width, y * gridblock.Height)), Color.Blue);
-            }
-        }
-    }
+			}
+		}
+	}
 }
